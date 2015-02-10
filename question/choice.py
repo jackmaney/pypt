2c8da62ec847feff5d502bab3eb38ddecb24da4d
Jack Maney <jackmaney@gmail.com>
2015-02-10 00:14:29 -0600

    Separated out classes of default questions and added a choice-based question

---
+++ question/choice.py
@@ -0,0 +1,62 @@
+from . import Question
+from util import get_input
+
+
+class ChoiceQuestion(Question):
+
+    def __init__(self, name, template_vars, description,
+                 choices, follow_ups, default=None):
+        self.description = description
+
+        if not choices:
+            raise ValueError("choices parameter must be a list of strings")
+
+        if default and default not in choices:
+            raise ValueError(
+                "default ({}) not found in the list of choices ({})".format(
+                    default, ",".join(choices)
+                ))
+
+        for choice in choices:
+            if choice != default and choice not in follow_ups:
+                raise ValueError(
+                    "Non-default choice {} needs a follow-up question".format(
+                        default
+                    ))
+        self.choices = choices
+        self.default = default
+        self.follow_ups = follow_ups
+
+        super(ChoiceQuestion, self).__init__(name, template_vars)
+
+    def _get_message(self):
+
+        choice_str = "[{}]".format(",".join(self.choices))
+
+        if self.default:
+            choice_str += "({}): "
+        else:
+            choice_str += ": "
+
+        return "{}:\n{}\n{}".format(self.name, self.description, choice_str)
+
+    def ask(self):
+
+        user_input = None
+
+        while not user_input:
+
+            user_input = get_input(self.message)
+
+            if not user_input and self.default:
+                user_input = self.default
+
+            if user_input in self.choices:
+                self.follow_ups[user_input].ask()
+                break
+            else:
+                user_input = None
+
+    def process(self):
+        # All processing is done by follow-up questions
+        pass
--- question/default.py
+++ question/default.py
@@ -1,7 +1,7 @@
 from . import Question
 
 
-class DefaultQuestion(Question):
+class SetupQuestion(Question):
 
     def __init__(self, name, template_vars, default=None, key_name=None):
         self.default = default
@@ -10,14 +10,14 @@ class DefaultQuestion(Question):
         else:
             self.key_name = name.lower()
 
-        super(DefaultQuestion, self).__init__(name, template_vars)
+        super(SetupQuestion, self).__init__(name, template_vars)
 
     def _get_message(self):
 
         if self.default:
             return "{} ({}): ".format(self.name, self.default)
         else:
-            return super(DefaultQuestion, self)._get_message()
+            return super(SetupQuestion, self)._get_message()
 
     def process(self, user_input):
 
@@ -30,3 +30,22 @@ class DefaultQuestion(Question):
         if user_input:
             self.template_vars["setup_kwargs"][
                 self.key_name] = "'{}'".format(user_input)
+
+
+class VariableQuestion(SetupQuestion):
+
+    def __init__(self, name, template_vars, variable_name,
+                 default=None, key_name=None):
+
+        self.variable_name = variable_name
+        super(VariableQuestion, self).__init__(name, template_vars,
+                                               default=default,
+                                               key_name=key_name)
+
+    def process(self, user_input):
+
+        if not user_input and self.default:
+            user_input = self.default
+
+        if user_input:
+            self.template_vars[self.variable_name] = "'{}'".format(user_input)
